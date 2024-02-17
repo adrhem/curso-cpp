833ebc3b517ded349cc5ab3f7457aec6dbf88851
robbieram <159855931+robbieram@users.noreply.github.com>
2024-02-16 18:11:20 -0600

    Update 4.condicion2.cpp

--- 4.condicion2.cpp
+++ 4.condicion2.cpp
@@ -1,17 +1,18 @@
-// Compilar y ejecutar: g++ .\4.condicion2.cpp && .\a.exe
+#include <iostream>
+using namespace std;
 
-/**
- * Realizar un programa que te pida el año y 
- * evaluar si es bisiesto o no.
- * 
- * Un año es bisiesto si es divisible entre 4,
- * excepto aquellos divisibles entre 100 pero no entre 400.
- * 
- * Ejemplo:
- * 2000 es bisiesto
- * 800 es bisiesto
- * 1900 no es bisiesto
- * 100 no es bisiesto
- * 2023 no es bisiesto
- *
-*/
+int main() {
+    int año=0;
+
+  cout<<"Este año es viciesto";
+  cin>>año;
+  if ( año % 4==0 && año % 100==0){
+    cout<< año << "si es viciesto";
+
+  }
+if ( año % 4!=0 && año % 100!=0){
+    cout<< año << "no es viciesto";
+
+  }
+  return 0;
+}
