08b40f6dd360d51cebcb6469fcc6b6970487bbbb
Adrián HM <4540334+adrhem@users.noreply.github.com>
2024-02-19 19:00:29 -0600

    tarea triangulo.

---
+++ 9.ciclos_triangulo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    int lado = 5;
+    std::string figura = "*";
+
+    /**
+        Imprimir un triángulo rectangulo usando la variable lado.
+
+        Salida: (5)
+        *
+        **
+        ***
+        ****
+        ***** 
+    */
+
+    return 0;
+}
