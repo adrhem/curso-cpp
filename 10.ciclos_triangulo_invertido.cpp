8b6993f146b962ef02e9196e21a8dd218b22ff6a
Adrián HM <4540334+adrhem@users.noreply.github.com>
2024-02-21 18:39:14 -0600

    triangulo invertido.

--- 10.ciclos_triangulo_invertido.cpp
+++ 10.ciclos_triangulo_invertido.cpp
@@ -9,16 +9,20 @@ int main() {
         Imprimir un triángulo rectangulo usando la variable lado.
 
         Salida: (5)
-        *
-        **
-        ***
-        ****
+            *
+           **
+          ***
+         ****
         ***** 
     */
     for (int i = 0; i < lado ; i++) {
-        for (int j = 0; j <= i ; j++) {
-            // std::cout << "i:" << i << " j:" << j << std::endl;
-            std::cout << figura;
+        for (int j = 0; j <= lado ; j++) {
+            // if (j < lado - i) {
+            //     std::cout << " ";
+            // } else {
+            //     std::cout << figura;
+            // }
+            std::cout << (j < lado - i ? " " : figura);
         }
         std::cout << std::endl;
     }
