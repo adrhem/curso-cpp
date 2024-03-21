65b48a5a484f1a80e2fe2882e4341edb7674c5d3
Julio Hernández <4540334+adrhem@users.noreply.github.com>
2024-03-20 18:51:35 -0600

    problema a resolver.

---
+++ 22.repartir.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int numero_hijas;
+    int monedas_sobrantes;
+
+    std::cin >> numero_hijas >> monedas_sobrantes;
+
+    return 0;
+}
+
+// Entrada 1: 2 5
+// Salida 1: 9 10
+
+// Entrada 2: 3 5
+// Salida 2: 7 7
