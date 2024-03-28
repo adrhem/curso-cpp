f3fb7db70869cfada45f9d45a51c937ca7f23948
Julio Hernández <4540334+adrhem@users.noreply.github.com>
2024-03-27 19:13:39 -0600

    repartir monedas.

--- 22.repartir.cpp
+++ 22.repartir.cpp
@@ -3,14 +3,18 @@
 int main() {
     int numero_hijas;
     int monedas_sobrantes;
+    int monedas_por_hija_maximas, monedas_por_hija_minimas, monedas_sobrantes_restantes;
 
     std::cin >> numero_hijas >> monedas_sobrantes;
 
+    monedas_por_hija_maximas = monedas_por_hija_minimas = monedas_sobrantes + monedas_sobrantes / (numero_hijas - 1);
+    monedas_sobrantes_restantes = monedas_sobrantes % (numero_hijas - 1);
+
+    if(monedas_sobrantes_restantes == 0) {
+        monedas_por_hija_minimas --;
+    }
+
+    std::cout << monedas_por_hija_minimas << " " << monedas_por_hija_maximas << std::endl;
+
     return 0;
 }
-
-// Entrada 1: 2 5
-// Salida 1: 9 10
-
-// Entrada 2: 3 5
-// Salida 2: 7 7
